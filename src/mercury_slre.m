20afc289982e91d37d7f2fa55f0059c7fe712d47
Sebastian Godelet <sebastian.godelet@outlook.com>
2015-05-16 17:59:22 +0800

    implemented stub

--- src/mercury_slre.m
+++ src/mercury_slre.m
@@ -18,22 +18,61 @@
 
 :- interface.
 
+:- import_module list.
+
 %----------------------------------------------------------------------------%
+
+:- type captures == list(string).
+
+:- type result
+    --->    ok(captures)
+    ;       error(match_error).
+
+:- type match_error
+    --->    no_match
+    ;       unexpected_quantifier
+    ;       unbalanced_brackets
+    ;       internal_error
+    ;       invalid_character_set
+    ;       invalid_metacharacter
+    ;       caps_array_too_small
+    ;       too_many_branches
+    ;       too_many_brackets.
+
+    % match(RegEx, Text, NumberOfCaptures) = Result.
+    %
+:- func match(string, string, int) = result.
 
 %----------------------------------------------------------------------------%
 %----------------------------------------------------------------------------%
 
 :- implementation.
 
+match(RegEx, Text, NumberOfCaptures) = Result :-
+    slre_match(RegEx, Text, NumberOfCaptures, ResultCode),
+    Result = ok([]).
+
 %----------------------------------------------------------------------------%
 %
-% Implemtation of server management functions and types in C
+% Implemtation of functions and types in C
 %
 
     % `include_file()' is preferred over `#include', since then linking
     % to the library does not require the presence of the API header
     %
 :- pragma foreign_decl("C", include_file("slre.h")).
+
+:- pred slre_match(string::in, string::in, NumberOfMatches::in,
+    int::out) is det.
+
+:- pragma foreign_proc("C",
+    slre_match(RegEx::in, Text::in, NumberOfCaptures::in, ResultCode::out),
+    [promise_pure, will_not_call_mercury],
+"
+    struct slre_cap captures[NumberOfCaptures];
+    ResultCode = slre_match(RegEx, Text, strlen(Text), captures,
+        NumberOfCaptures, 0);
+").
 
 %----------------------------------------------------------------------------%
 :- end_module mercury_slre.
